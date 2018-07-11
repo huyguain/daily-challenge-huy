adbb14abfcb1a00addc3e0c9d18459516959d5d3
huyguain <huyguainnt@gmail.com>
2018-07-11 09:48:41 +0700

    9-7

---
+++ daily-9-7/index1.spec.js
@@ -0,0 +1,21 @@
+function rotateMatrix (arr) {
+	const n = arr.length;
+	for (let c = 0; c < Math.floor(n/2); c++) {
+		const l = n - c - 1;
+		for (let i = 0; i < l - c; i++) {
+			const t 			= arr[l - i][c];	
+			arr[l - i][c] 	= arr[l][l - i];
+			arr[l][l - i] 	= arr[c + i][l];
+			arr[c + i][l] 	= arr[c][c + i];
+			arr[c][c + i] 	= t;
+		}
+	}
+	console.log(arr);
+}
+
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+rotateMatrix(arr)
