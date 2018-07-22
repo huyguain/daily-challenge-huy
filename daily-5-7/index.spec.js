927a83dd449d507771fcbe2f347ffbe45ffc8397
huyguain <huyguainnt@gmail.com>
2018-07-22 21:59:18 +0700

    rm

--- daily-5-7/index.spec.js
+++
@@ -1,9 +0,0 @@
-import { findNumber } from './index';
-
-
-describe('Test daily-5-7', () => {
-  it('should return', async () => {
-    const result = findNumber([-1,3,-2,-9,2,1]);
-    expect(result).toBe(4);
-  });
-});
